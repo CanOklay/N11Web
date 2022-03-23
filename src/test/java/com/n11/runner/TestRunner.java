12cf0cc33d73ce840964dc0f089725cea5aa290b
canoklay <fatihcan.oklay@vigo.ist>
2022-03-23 10:16:39 +0300

    cucumber setup

--- src/test/java/com/n11/runner/TestRunner.java
+++ src/test/java/com/n11/runner/TestRunner.java
@@ -1,12 +1,13 @@
 package com.n11.runner;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/java/com/n11/features",
-        glue = {"stepImplementation"},
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber-reports/cucumber.html",
+                            "json:target/cucumber-reports/cucumber.json"},
+        features="src/test/resources/features",
+        glue = {"com.n11.stepImplementation"},
         monochrome = true)
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests {
 }
--- src/test/java/com/n11/stepImplementation/LoginTest.java
+++ src/test/java/com/n11/stepImplementation/LoginTest.java
@@ -1,29 +1,44 @@
 package com.n11.stepImplementation;
 
-import com.n11.page.BasePage;
-import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class LoginTest extends BasePage {
+public class LoginTest {
 
-    Logger logger = LogManager.getLogger(com.n11.stepImplementation.LoginTest.class.getName());
+    protected static WebDriver driver;
+    protected static WebDriverWait webDriverWait;
 
-    @Given("user is on login page")
+    @Given("^user is on login page$")
     public void user_is_on_login_page() {
-        System.out.println("asdb");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("disable-popup-blocking");
+        options.addArguments("disable-translate");
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        driver.get("https://www.n11.com/");
+        driver.quit();
     }
+
     @When("user enters username and password")
     public void user_enters_username_and_password() {
         System.out.println("asdb");
     }
+
     @And("clicks on login button")
     public void clicks_on_login_button() {
         System.out.println("asdb");
     }
+
     @Then("user is navigated to the homepage")
     public void user_is_navigated_to_the_homepage() {
         System.out.println("asdb");
